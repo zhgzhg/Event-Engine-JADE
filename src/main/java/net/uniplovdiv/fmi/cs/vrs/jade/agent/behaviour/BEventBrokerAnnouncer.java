f9602fb194d0d7eee2d1736429d1cf6232d9f834
Zh G <perkoz@mail.bg>
2018-08-23 01:35:06 +0300

    BEventBrokerAnnouncer works with BasicConfiguration instance instead of topic. Upon wake it's able to notice the latest values there.

--- src/main/java/net/uniplovdiv/fmi/cs/vrs/jade/agent/behaviour/BEventBrokerAnnouncer.java
+++ src/main/java/net/uniplovdiv/fmi/cs/vrs/jade/agent/behaviour/BEventBrokerAnnouncer.java
@@ -5,6 +5,8 @@ import jade.domain.FIPAAgentManagement.Property;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.util.leap.Iterator;
 import net.uniplovdiv.fmi.cs.vrs.jade.agent.EventBrokerAgent;
+import net.uniplovdiv.fmi.cs.vrs.jade.agent.configuration.BasicConfiguration;
+import net.uniplovdiv.fmi.cs.vrs.jade.agent.util.ServiceDescriptionUtils;
 import net.uniplovdiv.fmi.cs.vrs.jade.agent.util.YellowPagesUtils;
 
 import java.util.Objects;
@@ -14,37 +16,43 @@ import java.util.Objects;
  * ar agent is an event distributor (broker).
  */
 public class BEventBrokerAnnouncer extends WakerBehaviour {
-    private static final long serialVersionUID = -177964163377930581L;
+    private static final long serialVersionUID = -1373575430029846294L;
 
     private YellowPagesUtils yup;
+    private BasicConfiguration brokerCfg;
 
     /**
      * Constructor of a Waker behaviour type used to announce a event distribution service via directory facilitator.
      * @param yup Initialized YellowPagesUtils class. Cannot be null.
-     * @param eventTopic The event types (category) the associated JADE agent is going to interact with. Cannot be
-     *                   null or empty.
+     * @param brokerCfg The configuration used for initializing a particular broker instance.
      * @throws NullPointerException If yup or eventTopic or the agent instance inside yup is null.
      * @throws IllegalArgumentException If eventTopic is empty.
      */
-    public BEventBrokerAnnouncer(YellowPagesUtils yup, String eventTopic) {
+    public BEventBrokerAnnouncer(YellowPagesUtils yup, BasicConfiguration brokerCfg) {
         super(yup.getAgent(), 2000L);
-        Objects.requireNonNull(eventTopic, "Null event topic");
-        if (eventTopic.isEmpty()) throw new IllegalArgumentException("Empty string for eventTopic is not allowed");
+        Objects.requireNonNull(brokerCfg, "Null broker configuration provided");
+        if (brokerCfg.topic.isEmpty())
+            throw new IllegalArgumentException("Empty string for topic in brokerCfg is not allowed");
         this.yup = yup;
+        this.brokerCfg = brokerCfg;
 
         EventBrokerAgent.registerLangFipaSLIfMissing(yup.getAgent());
-
-        ServiceDescription sd = yup.getServiceDescription();
-
-        for (Iterator it = sd.getAllProperties(); it.hasNext(); ) {
-            Property p = (Property) it.next();
-            if (p.getName().equals("event-topic")) it.remove();
-        }
-        sd.addProperties(new Property("event-topic", eventTopic));
     }
 
     @Override
     protected void onWake() {
-        yup.register();
+        if (this.brokerCfg.topic.isEmpty())
+            throw new IllegalArgumentException("Empty string for topic in brokerCfg is not allowed");
+
+        ServiceDescription sd = this.yup.getServiceDescription();
+
+        ServiceDescriptionUtils.setFirstPropertyNamed(
+                ServiceDescriptionUtils.EVENT_TOPIC_PROP, this.brokerCfg.topic, sd);
+
+        ServiceDescriptionUtils.setFirstPropertyNamed(
+                ServiceDescriptionUtils.MAX_TIME_WITHOUT_BROKER_CONNECTION_MILLIS_PROP,
+                this.brokerCfg.maxTimeWithoutBrokerConnectionMillis, sd);
+
+        this.yup.register();
     }
 }
