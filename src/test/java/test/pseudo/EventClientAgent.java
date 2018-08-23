0a38772e290695d41eb33259f6fff4774a23b63e
Zh G <perkoz@mail.bg>
2018-08-24 02:12:25 +0300

    Example for subscr sync time change in the subscr behaviour based on the remote broker information

--- src/test/java/test/pseudo/EventClientAgent.java
+++ src/test/java/test/pseudo/EventClientAgent.java
@@ -3,9 +3,15 @@ package test.pseudo;
 import jade.core.Agent;
 import jade.core.behaviours.ThreadedBehaviourFactory;
 import jade.core.behaviours.WakerBehaviour;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.Property;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.util.leap.Iterator;
 import net.uniplovdiv.fmi.cs.vrs.event.Event;
+import net.uniplovdiv.fmi.cs.vrs.jade.agent.EventBrokerAgent;
 import net.uniplovdiv.fmi.cs.vrs.jade.agent.behaviour.BEventBrokerSubscriber;
 import net.uniplovdiv.fmi.cs.vrs.jade.agent.behaviour.BEventChannel;
+import net.uniplovdiv.fmi.cs.vrs.jade.agent.ontology.EventEngineOntology;
 import net.uniplovdiv.fmi.cs.vrs.jade.agent.ontology.SubscriptionParameter;
 import net.uniplovdiv.fmi.cs.vrs.jade.agent.util.ServiceDescriptionUtils;
 import net.uniplovdiv.fmi.cs.vrs.jade.agent.util.YellowPagesUtils;
@@ -27,6 +33,36 @@ public class EventClientAgent extends Agent {
             protected void onWake() {
                 BEventBrokerSubscriber bbs = new BEventBrokerSubscriber(new YellowPagesUtils(getAgent(),
                         ServiceDescriptionUtils.createEventSourceSD(null)), null);
+
+                // an example how to change the subscription timeout preference of the behaviour
+                // based on the info provided by the broker
+                bbs.setSubscribeTimeoutCalculator((maxTime, agentDescr) -> {
+                    if (!agentDescr.isPresent()) return null;
+
+                    DFAgentDescription dfAgentDescription = agentDescr.get();
+                    for (Iterator it = dfAgentDescription.getAllServices(); it.hasNext(); ) {
+                        ServiceDescription sd = (ServiceDescription) it.next();
+
+                        if (sd == null || !ServiceDescriptionUtils.SERVICE_NAME.equals(sd.getName())
+                                || !ServiceDescriptionUtils.SERVICE_TYPE.equals(sd.getType()))
+                            continue;
+
+                        Property prop = ServiceDescriptionUtils.findFirstPropertyNamed(
+                                ServiceDescriptionUtils.MAX_TIME_WITHOUT_BROKER_CONNECTION_MILLIS_PROP, sd);
+                        if (prop != null) {
+                            try {
+                                Long l = Long.parseLong(prop.getValue().toString());
+                                return (l <= 300000 ? l : null); // we don't want to block too much
+                            } catch (Exception e) {
+                                e.printStackTrace(System.err);
+                            }
+                        }
+                        break;
+                    }
+
+                    return null;
+                });
+
                 addBehaviour(tbf.wrap(bbs));
 
                 BEventChannel bes = new BEventChannel(getAgent(), 300, bbs, (event) ->
